d8d068ee424be594bf4122451da62f9507521540
Jeremy Green <jeremygreen@MacBook-Pro.local>
2015-12-08 09:23:59 -0700

    Move js and ajax functionality for multiple levels to add on

--- functions.php
+++ functions.php
@@ -1855,32 +1855,6 @@ if ( !function_exists( 'build_leaky_paywall_subscription_row_post_type' ) ) {
 	}
 	
 }
- 
-if ( !function_exists( 'build_leaky_paywall_subscription_row_post_type_ajax' ) ) {
-
-	/**
-	 * AJAX Wrapper
-	 *
-	 * @since 1.0.0
-	 */
-	function build_leaky_paywall_subscription_row_post_type_ajax() {
-	
-		if ( isset( $_REQUEST['select-post-key'] ) && isset( $_REQUEST['row-key'] ) ) {
-			$settings = get_leaky_paywall_settings();
-			
-			if ( is_multisite_premium() && preg_match( '#^' . network_admin_url() . '#i', $_SERVER['HTTP_REFERER'] ) ) {
-				if ( !defined( 'WP_NETWORK_ADMIN' ) ) {
-					define( 'WP_NETWORK_ADMIN', true );
-				}
-			}
-			
-			die( build_leaky_paywall_subscription_row_post_type( array(), $_REQUEST['select-post-key'], $_REQUEST['row-key'] ) );
-		}
-		die();
-	}
-	add_action( 'wp_ajax_issuem-leaky-paywall-add-new-subscription-row-post-type', 'build_leaky_paywall_subscription_row_post_type_ajax' );
-	
-}
 
 if ( !function_exists( 'build_leaky_paywall_default_restriction_row' ) ) {
 
--- js/issuem-leaky-paywall-settings.js
+++ js/issuem-leaky-paywall-settings.js
@@ -13,17 +13,6 @@ $leaky_paywall_settings(document).ready(function($) {
         });
 	});
 	
-	$( '#leaky_paywall_subscription_level_options' ).on( 'click', 'input#add-subscription-row', function( event ) {
-		event.preventDefault();
-        var data = {
-            'action': 'issuem-leaky-paywall-add-new-subscription-row',
-            'row-key': ++leaky_paywall_subscription_levels_row_key,
-        }
-        $.post( ajaxurl, data, function( response ) {
-            $( 'td#issuem-leaky-paywall-subscription-level-rows' ).append( response );
-        });
-	});
-	
 	$( '#leaky_paywall_subscription_level_options' ).on( 'click', 'input#add-subscription-row-post-type', function( event ) {
 		event.preventDefault();
 		var row_key = $( this ).data( 'row-key' );
