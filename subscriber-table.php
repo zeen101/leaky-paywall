64bc4e515589b5a41201c53e0003fd7082dd0601
Jeremy Green <greenhornet79@gmail.com>
2017-11-28 11:12:01 -0700

    display wordpress users who do not have a payment gateway in the subscriber table if all wpusers are being queried

--- subscriber-table.php
+++ subscriber-table.php
@@ -209,9 +209,12 @@ class Leaky_Paywall_Subscriber_List_Table extends WP_List_Table {
 					$payment_gateway = get_user_meta( $user->ID, '_issuem_leaky_paywall_' . $mode . '_payment_gateway', true );
 				}
 				
-				if ( empty( $payment_gateway ) ) {
-					continue;
+				if ( !empty( $_GET['user-type'] ) && 'wpusers' !== $_GET['user-type'] ) {
+					if ( empty( $payment_gateway ) ) {
+						continue;
+					}
 				}
+				
 				?>
 				<tr class="<?php echo $alt; ?>">
 				<?php
