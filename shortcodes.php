cde67f1d8bbdc1f9722ef6ab38ff62fc410dfe92
Jeremy Green <greenhornet79@gmail.com>
2019-04-09 16:26:56 -0600

    update cancel language

--- shortcodes.php
+++ shortcodes.php
@@ -430,8 +430,8 @@ if ( !function_exists( 'do_leaky_paywall_profile' ) ) {
 					}
 				}
 			} else if ( !empty( $plan ) && 'Canceled' == $plan ) {
-				$results .= '<h2 class="leaky-paywall-subscription-status-header">' . __( 'Your Subscription Has Been Cancelled', 'leaky-paywall' ) . '</h2>';
-				$results .= '<p>' . sprintf( __( 'You have cancelled your subscription, but your account will remain active until your expiration date. To reactivate your subscription, please visit our <a href="%s">Subscription page</a>.', 'leaky-paywall' ), get_page_link( $settings['page_for_subscription'] ) ) . '</p>';
+				$results .= '<h2 class="leaky-paywall-subscription-status-header">' . __( 'Your Subscription Has Been Canceled', 'leaky-paywall' ) . '</h2>';
+				$results .= '<p>' . sprintf( __( 'You have canceled your subscription, but your account will remain active until your expiration date. To reactivate your subscription, please visit our <a href="%s">Subscription page</a>.', 'leaky-paywall' ), get_page_link( $settings['page_for_subscription'] ) ) . '</p>';
 			} else {
 				
 				$results .= '<h2 class="leaky-paywall-subscription-status-header">' . __( 'Your Account is Not Currently Active', 'leaky-paywall' ) . '</h2>';
