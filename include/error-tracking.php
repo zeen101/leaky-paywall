8fa0565f79a6ca4c25cd1b8780e1517140fc02fe
greenhornet79 <greenhornet79@gmail.com>
2021-10-21 08:44:40 -0600

    code cleanup

--- include/error-tracking.php
+++ include/error-tracking.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
 
-/**
- * Stores error messages
- *
- * @access      public
- * @since       1.0
- */
-if ( !function_exists( 'leaky_paywall_errors' ) ) {
+if ( ! function_exists( 'leaky_paywall_errors' ) ) {
+	/**
+	 * Stores error messages
+	 *
+	 * @since       1.0
+	 */
 	function leaky_paywall_errors() {
-	    static $wp_error; // Will hold global variable safely
-	    return isset( $wp_error ) ? $wp_error : ( $wp_error = new WP_Error( NULL, NULL, NULL ) );
+		static $wp_error; // Will hold global variable safely.
+		return isset( $wp_error ) ? $wp_error : ( $wp_error = new WP_Error( null, null, null ) );
 	}
 }
 
@@ -24,20 +23,19 @@ function leaky_paywall_get_error_messages_html( $error_id = '' ) {
 	$html   = '';
 	$errors = leaky_paywall_errors()->get_error_codes();
 
-	if( $errors ) {
-		
-		$html .= '<div class="leaky_paywall_message error">';
-		// Loop error codes and display errors
-		foreach( $errors as $code ) {
+	if ( $errors ) {
 
-			if( leaky_paywall_errors()->get_error_data( $code ) == $error_id ) {
+		$html .= '<div class="leaky_paywall_message error">';
+		// Loop error codes and display errors.
+		foreach ( $errors as $code ) {
+
+			if ( leaky_paywall_errors()->get_error_data( $code ) == $error_id ) {
 
 				$message = leaky_paywall_errors()->get_error_message( $code );
 
 				$html .= '<p class="leaky_paywall_error ' . esc_attr( $code ) . '"><span>' . $message . '</span></p>';
 
 			}
-
 		}
 
 		$html .= '</div>';
@@ -53,10 +51,12 @@ function leaky_paywall_get_error_messages_html( $error_id = '' ) {
  *
  * @access      public
  * @since       4.0.0
+ *
+ * @param string $error_id The error id.
  */
 function leaky_paywall_show_error_messages( $error_id = '' ) {
 
-	if( leaky_paywall_errors()->get_error_codes() ) {
+	if ( leaky_paywall_errors()->get_error_codes() ) {
 
 		do_action( 'leaky_paywall_errors_before' );
 		echo leaky_paywall_get_error_messages_html( $error_id );
--- include/license-key.php
+++ include/license-key.php
@@ -1,34 +1,53 @@
 <?php
 /**
- * Registers zeen101's Leaky Paywall License Key class
+ * Registers Leaky Paywall License Key class
  *
- * @package zeen101's Leaky Paywall
+ * @package Leaky Paywall
  * @since 4.4.0
  */
 
-/**
- * This class registers the Leaky Paywall license key activate/deactive functionality
- *
- * @since 4.4.0
- */
 if ( ! class_exists( 'Leaky_Paywall_License_Key' ) ) {
-	
+	/**
+	 * This class registers the Leaky Paywall license key activate/deactive functionality
+	 *
+	 * @since 4.4.0
+	 */
 	class Leaky_Paywall_License_Key {
-		
+
+		/**
+		 * The plugin slug
+		 *
+		 * @var string
+		 */
 		private $plugin_slug;
-		private $plugin_prefix; // underscored version of slug
+
+		/**
+		 * The underscored version of the slug
+		 *
+		 * @var string
+		 */
+		private $plugin_prefix;
+
+		/**
+		 * The plugin name
+		 *
+		 * @var string
+		 */
 		private $plugin_name;
-		
+
 		/**
 		 * Class constructor, puts things in motion
 		 *
 		 * @since 4.4.0
+		 *
+		 * @param string $plugin_slug The plugin slug.
+		 * @param string $plugin_name The plugin name.
 		 */
-		function __construct( $plugin_slug, $plugin_name ) {
+		public function __construct( $plugin_slug, $plugin_name ) {
 
-			$this->plugin_slug = $plugin_slug;
-			$this->plugin_name = $plugin_name;
-			$this->plugin_prefix = str_replace('-', '_', $plugin_slug );
+			$this->plugin_slug   = $plugin_slug;
+			$this->plugin_name   = $plugin_name;
+			$this->plugin_prefix = str_replace( '-', '_', $plugin_slug );
 
 			add_action( 'admin_init', array( $this, 'activate_license' ) );
 			add_action( 'admin_init', array( $this, 'deactivate_license' ) );
@@ -43,24 +62,26 @@ if ( ! class_exists( 'Leaky_Paywall_License_Key' ) ) {
 		 * @since 4.4.0
 		 */
 		public function get_settings() {
-			
-			$defaults = array( 
-				'license_key'					=> '',
-				'license_status'				=> '',
+
+			$defaults = array(
+				'license_key'    => '',
+				'license_status' => '',
 			);
-		
+
 			$defaults = apply_filters( $this->plugin_slug . '_default_license_settings', $defaults );
 			$settings = get_option( $this->plugin_slug );
 			$settings = wp_parse_args( $settings, $defaults );
 
 			return apply_filters( $this->plugin_prefix . '_get_license_settings', $settings );
-			
+
 		}
 
 		/**
 		 * Update Leaky Paywall License Settings for this plugin
 		 *
 		 * @since 4.4.0
+		 *
+		 * @param array $settings The settings.
 		 */
 		public function update_settings( $settings ) {
 			update_option( $this->plugin_slug, $settings );
@@ -78,53 +99,54 @@ if ( ! class_exists( 'Leaky_Paywall_License_Key' ) ) {
 			?>
 			<div id="modules" class="postbox">
 
-				<h3 class="hndle"><span><?php _e( $this->plugin_name, $this->plugin_slug ); ?></span></h3>
+				<h3 class="hndle"><span><?php esc_attr_e( $this->plugin_name, $this->plugin_slug ); ?></span></h3>
 
-                <div class="inside">
-                    
-                    <table id="<?php echo $this->plugin_prefix; ?>_license_key" class="form-table">
-                    	<tr>
-                            <th rowspan="1"> 
-                            	<?php _e( 'License Key', $this->plugin_slug ); ?>
-                            </th>
+				<div class="inside">
+					
+					<table id="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_key" class="form-table">
+						<tr>
+							<th rowspan="1"> 
+								<?php esc_attr_e( 'License Key', $this->plugin_slug ); ?>
+							</th>
 	
-                            <td>
-                            	<input type="text" id="<?php echo $this->plugin_prefix; ?>_license_key" class="regular-text" name="<?php echo $this->plugin_prefix; ?>_license_key" value="<?php echo htmlspecialchars( stripcslashes( $settings['license_key'] ) ); ?>" />
-                            
-	                            <?php if( $settings['license_status'] !== false 
-										&& $settings['license_status'] == 'valid' ) { 
-	                            		// license is active
-								?>
-											<span style="color:green;"><?php _e('active'); ?></span>
+							<td>
+								<input type="text" id="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_key" class="regular-text" name="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_key" value="<?php echo esc_attr( $settings['license_key'] ); ?>" />
+							
+								<?php
+								if ( false !== $settings['license_status']
+										&& 'valid' == $settings['license_status'] ) {
+														   // license is active.
+									?>
+											<span style="color:green;"><?php esc_attr_e( 'active' ); ?></span>
 											<input type="submit" class="button-secondary" 
-												name="<?php echo $this->plugin_prefix; ?>_license_deactivate" 
-												value="<?php _e( 'Deactivate License', $this->plugin_slug ); ?>"/>
+												name="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_deactivate" 
+												value="<?php esc_attr_e( 'Deactivate License', $this->plugin_slug ); ?>"/>
 
-								<?php } else if ( $settings['license_status'] == 'invalid' ) {	
-										// license is invalid
-								?>
-											<span style="color:red;"><?php _e('invalid'); ?></span>
+									<?php
+								} elseif ( 'invalid' == $settings['license_status'] ) {
+										// license is invalid.
+									?>
+											<span style="color:red;"><?php esc_attr_e( 'invalid' ); ?></span>
 											<input type="submit" class="button-secondary" 
-												name="<?php echo $this->plugin_prefix; ?>_license_activate" 
-												value="<?php _e( 'Activate License', $this->plugin_slug ); ?>"/>
+												name="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_activate" 
+												value="<?php esc_attr_e( 'Activate License', $this->plugin_slug ); ?>"/>
 
-								<?php } else  { 
-										// license hasn't been entered yet
+									<?php
+								} else {
+										// license hasn't been entered yet.
 									?>
 											<input type="submit" class="button-secondary" 
-												name="<?php echo $this->plugin_prefix; ?>_license_activate" 
-												value="<?php _e( 'Activate License', $this->plugin_slug ); ?>"/>
+												name="<?php echo esc_attr( $this->plugin_prefix ); ?>_license_activate" 
+												value="<?php esc_attr_e( 'Activate License', $this->plugin_slug ); ?>"/>
 
 								<?php } ?>
-	                            
-                            </td>
-                        </tr>
-                    </table>
-
-                </div>
-                    
-            </div>
-			<?php 
+								
+							</td>
+						</tr>
+					</table>
+				</div>	
+			</div>
+			<?php
 		}
 
 		/**
@@ -132,45 +154,52 @@ if ( ! class_exists( 'Leaky_Paywall_License_Key' ) ) {
 		 *
 		 * @since 4.4.0
 		 */
-		function activate_license() {
+		public function activate_license() {
 
-			if ( !isset( $_POST[$this->plugin_prefix . '_license_activate'] ) ) {
+			if ( ! isset( $_POST[ $this->plugin_prefix . '_license_activate' ] ) ) {
 				return;
 			}
 
-			if( ! check_admin_referer( 'verify', 'leaky_paywall_license_wpnonce' ) ) {
+			if ( ! check_admin_referer( 'verify', 'leaky_paywall_license_wpnonce' ) ) {
 				return;
 			}
 
 			$settings = $this->get_settings();
 
-			if ( !empty( $_POST[$this->plugin_prefix . '_license_key'] ) ) {
-				$settings['license_key'] = $_POST[$this->plugin_prefix . '_license_key'];
+			if ( ! empty( $_POST[ $this->plugin_prefix . '_license_key' ] ) ) {
+				$settings['license_key'] = sanitize_text_field( wp_unslash( $_POST[ $this->plugin_prefix . '_license_key' ] ) );
 			}
 
 			$license = trim( $settings['license_key'] );
-	
-			// data to send in our API request
-			$api_params = array( 
-				'edd_action'=> 'activate_license', 
-				'license' 	=> $license, 
-				'item_name' => urlencode( $this->plugin_name ) // the name of our product in EDD
-			);
-			
-			// Call the custom API.
-			$response = wp_remote_get( esc_url_raw( add_query_arg( $api_params, ZEEN101_STORE_URL ) ), array( 'timeout' => 15, 'sslverify' => false ) );
 
-			// make sure the response came back okay
-			if ( is_wp_error( $response ) )
+			// data to send in our API request.
+			$api_params = array(
+				'edd_action' => 'activate_license',
+				'license'    => $license,
+				'item_name'  => rawurlencode( $this->plugin_name ), // the name of our product in EDD.
+			);
+
+			// Call the custom API.
+			$response = wp_remote_get(
+				esc_url_raw( add_query_arg( $api_params, ZEEN101_STORE_URL ) ),
+				array(
+					'timeout'   => 15,
+					'sslverify' => false,
+				)
+			);
+
+			// make sure the response came back okay.
+			if ( is_wp_error( $response ) ) {
 				return false;
-	
-			// decode the license data
+			}
+
+			// decode the license data.
 			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-			
-			// $license_data->license will be either "active" or "inactive"
+
+			// $license_data->license will be either "active" or "inactive".
 			$settings['license_status'] = $license_data->license;
 			$this->update_settings( $settings );
-			
+
 		}
 
 		/**
@@ -179,47 +208,53 @@ if ( ! class_exists( 'Leaky_Paywall_License_Key' ) ) {
 		 * @since 4.4.0
 		 */
 		public function deactivate_license() {
-			
-			if( !isset( $_POST[$this->plugin_prefix . '_license_deactivate'] ) ) {
+
+			if ( ! isset( $_POST[ $this->plugin_prefix . '_license_deactivate' ] ) ) {
 				return;
 			}
 
-			if( ! check_admin_referer( 'verify', 'leaky_paywall_license_wpnonce' ) ) {
-				return; 
+			if ( ! check_admin_referer( 'verify', 'leaky_paywall_license_wpnonce' ) ) {
+				return;
 			}
-			
-			$settings = $this->get_settings();
-	
-			// retrieve the license from the database
-			$license = trim( $settings['license_key'] );
-	
-			// data to send in our API request
-			$api_params = array( 
-				'edd_action'=> 'deactivate_license', 
-				'license' 	=> $license, 
-				'item_name' => urlencode( $this->plugin_name ) // the name of our product in EDD
-			);
-			
-			// Call the custom API.
-			$response = wp_remote_get( esc_url_raw( add_query_arg( $api_params, ZEEN101_STORE_URL ) ), array( 'timeout' => 15, 'sslverify' => false ) );
 
-			// make sure the response came back okay
+			$settings = $this->get_settings();
+
+			// retrieve the license from the database.
+			$license = trim( $settings['license_key'] );
+
+			// data to send in our API request.
+			$api_params = array(
+				'edd_action' => 'deactivate_license',
+				'license'    => $license,
+				'item_name'  => rawurlencode( $this->plugin_name ), // the name of our product in EDD.
+			);
+
+			// Call the custom API.
+			$response = wp_remote_get(
+				esc_url_raw( add_query_arg( $api_params, ZEEN101_STORE_URL ) ),
+				array(
+					'timeout'   => 15,
+					'sslverify' => false,
+				)
+			);
+
+			// make sure the response came back okay.
 			if ( is_wp_error( $response ) ) {
 				return false;
 			}
-	
-			// decode the license data
+
+			// decode the license data.
 			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-			
-			// $license_data->license will be either "deactivated" or "failed"
-			if ( $license_data->license == 'deactivated' || $license_data->license == 'failed' ) {
-				
+
+			// $license_data->license will be either "deactivated" or "failed".
+			if ( 'deactivated' == $license_data->license || 'failed' == $license_data->license ) {
+
 				unset( $settings['license_key'] );
 				unset( $settings['license_status'] );
 				$this->update_settings( $settings );
-				
+
 			}
-		
+
 		}
 
 	}
