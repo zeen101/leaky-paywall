910efa9ff2e319ffa761ff05a608c46d0c3fc91d
greenhornet79 <greenhornet79@gmail.com>
2022-09-08 10:45:26 -0600

    add fix for logging Stripe charge id on transaction

--- include/gateways/class-leaky-paywall-payment-gateway-stripe.php
+++ include/gateways/class-leaky-paywall-payment-gateway-stripe.php
@@ -214,11 +214,11 @@ class Leaky_Paywall_Payment_Gateway_Stripe extends Leaky_Paywall_Payment_Gateway
 
 			case 'charge.succeeded':
 				update_user_meta( $user->ID, '_issuem_leaky_paywall_' . $mode . '_payment_status' . $site, 'active' );
+
 				$transaction_id = leaky_paywall_get_transaction_id_from_email( $user->user_email );
-				if ( !isset( $stripe_object->billing_reason ) ) {
-					leaky_paywall_set_payment_transaction_id( $transaction_id, $stripe_object->id );
-				} else if ( isset( $stripe_object->billing_reason ) && 'subscription_cycle' !== $stripe_object->billing_reason ) {
-					// do not update transaction id if a recurring payment.  A new LP transaction will be created instead.
+		
+				// only recurring payments will include the word "Invoice"
+				if ( strpos( $stripe_object->description, 'Invoice' ) === false ) {
 					leaky_paywall_set_payment_transaction_id( $transaction_id, $stripe_object->id );
 				}
 				
